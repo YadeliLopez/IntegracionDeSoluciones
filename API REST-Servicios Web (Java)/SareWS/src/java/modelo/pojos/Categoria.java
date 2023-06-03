114bba0142342a8b34280801a8fa4cef63a913dd
YadeliLopez <89539688+YadeliLopez@users.noreply.github.com>
2023-06-02 21:05:23 -0600

    API REST-Servicios Web (Java)

---
+++ API REST-Servicios Web (Java)/SareWS/src/java/modelo/pojos/Categoria.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.pojos;
+
+/**
+ *
+ * @author Yadelí López
+ */
+public class Categoria {
+    public int idCategoria;
+    public String nombre;
+    public String activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(int idCategoria, String nombre, String activo) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.activo = activo;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getActivo() {
+        return activo;
+    }
+
+    public void setActivo(String activo) {
+        this.activo = activo;
+    }
+}
---
+++ API REST-Servicios Web (Java)/SareWS/src/java/modelo/pojos/Usuario.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.pojos;
+
+/**
+ *
+ * @author Yadelí López
+ */
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String celular;
+    private String usuario;
+    private String contrasena;
+    private int idRol;
+    private String rol;
+    private int idEstatus;
+    private String estatus;
+    private int idRancho;
+    private String rancho;
+    private int idUsuarioAlta;
+    private String usuarioAlta;
+    private String fechaAlta;
+    private int idUsuarioEditor;
+    private String usuarioEditor;
+    private String fechaEdicion;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellidoPaterno, String apellidoMaterno, String celular, String usuario, String contrasena, int idRol, int idEstatus, int idRancho, int idUsuarioAlta, String fechaAlta, int idUsuarioEditor, String fechaEdicion) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.celular = celular;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.idRol = idRol;
+        this.idEstatus = idEstatus;
+        this.idRancho = idRancho;
+        this.idUsuarioAlta = idUsuarioAlta;
+        this.fechaAlta = fechaAlta;
+        this.idUsuarioEditor = idUsuarioEditor;
+        this.fechaEdicion = fechaEdicion;
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellidoPaterno, String apellidoMaterno, String celular, String usuario, String contrasena, int idRol, String rol, int idEstatus, String estatus, int idRancho, String rancho, int idUsuarioAlta, String usuarioAlta, String fechaAlta, int idUsuarioEditor, String usuarioEditor, String fechaEdicion) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.celular = celular;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.idRol = idRol;
+        this.rol = rol;
+        this.idEstatus = idEstatus;
+        this.estatus = estatus;
+        this.idRancho = idRancho;
+        this.rancho = rancho;
+        this.idUsuarioAlta = idUsuarioAlta;
+        this.usuarioAlta = usuarioAlta;
+        this.fechaAlta = fechaAlta;
+        this.idUsuarioEditor = idUsuarioEditor;
+        this.usuarioEditor = usuarioEditor;
+        this.fechaEdicion = fechaEdicion;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public int getIdEstatus() {
+        return idEstatus;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public int getIdRancho() {
+        return idRancho;
+    }
+
+    public String getRancho() {
+        return rancho;
+    }
+
+    public int getIdUsuarioAlta() {
+        return idUsuarioAlta;
+    }
+
+    public String getUsuarioAlta() {
+        return usuarioAlta;
+    }
+
+    public String getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public int getIdUsuarioEditor() {
+        return idUsuarioEditor;
+    }
+
+    public String getUsuarioEditor() {
+        return usuarioEditor;
+    }
+
+    public String getFechaEdicion() {
+        return fechaEdicion;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public void setIdEstatus(int idEstatus) {
+        this.idEstatus = idEstatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public void setIdRancho(int idRancho) {
+        this.idRancho = idRancho;
+    }
+
+    public void setRancho(String rancho) {
+        this.rancho = rancho;
+    }
+
+    public void setIdUsuarioAlta(int idUsuarioAlta) {
+        this.idUsuarioAlta = idUsuarioAlta;
+    }
+
+    public void setUsuarioAlta(String usuarioAlta) {
+        this.usuarioAlta = usuarioAlta;
+    }
+
+    public void setFechaAlta(String fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public void setIdUsuarioEditor(int idUsuarioEditor) {
+        this.idUsuarioEditor = idUsuarioEditor;
+    }
+
+    public void setUsuarioEditor(String usuarioEditor) {
+        this.usuarioEditor = usuarioEditor;
+    }
+
+    public void setFechaEdicion(String fechaEdicion) {
+        this.fechaEdicion = fechaEdicion;
+    }
+}
